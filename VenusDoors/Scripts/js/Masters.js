ed09a661db86c66cf84f2c9aef4a801e1c7d1a11
xavierso <javier.sotillo13@gmail.com>
2019-01-14 09:55:44 -0400

    Masters e insertar

---
+++ VenusDoors/Scripts/js/Masters.js
@@ -0,0 +1,43 @@
+﻿$(document).ready(function () {
+    $("#btInsert").on("click", function () {
+        InsertBottomRail();
+    });
+});
+
+function InsertBottomRail() {
+
+    var datos =
+    {
+        pBottomRail: {
+            Description: $("#inDescription").val(),
+            Status: { Id: $("#inStatus").val() },
+            CreatorUser: 6,
+            ModificationUser: 6,
+
+
+
+
+        }
+    };
+    console.log(datos);
+    $.ajax({
+        type: 'POST',
+        data: JSON.stringify(datos),
+        url: urlInsertBottomRail,
+        dataType: "json",
+        contentType: 'application/json; charset=utf-8',
+        success: function (result) {
+
+            //Validar data para ver si mostrar error al guardar o exito al guardar
+            if (result == true) {
+                alert("yes");
+            } else {
+                alert("no");
+            }
+        },
+        error: function (err) {
+            alert("error");
+        },
+
+    });
+}
