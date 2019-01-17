0efd2e19df6eb99af3f606185638ee79820fe0dc
sotillo257 <sotillo257@gmail.com>
2019-01-16 20:02:33 -0400

    Merch

--- VenusDoors/Controllers/MastersController.cs
+++ VenusDoors/Controllers/MastersController.cs
@@ -83,16 +83,16 @@ namespace VenusDoors.Controllers
             }
         }
 
-        public ActionResult DeleteBottomRail(int id)
-        {
+        //public ActionResult DeleteBottomRail(int id)
+        //{
 
-            BottomRail dBottomRail = ViewBag.delBottomRail(id);
+        //    BottomRail dBottomRail = ViewBag.delBottomRail(id);
 
-            if (dBottomRail == null)
-                return View("NotFound");
-            else
-                return View(dBottomRail);
-        }
+        //    if (dBottomRail == null)
+        //        return View("NotFound");
+        //    else
+        //        return View(dBottomRail);
+        //}
 
 
         public ActionResult Doors()
