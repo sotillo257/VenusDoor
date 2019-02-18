ced577e736ce1db3de3ae0671dc09c4e8fa0251d
gregorisalazar <gregorisalazar.a@gmail.com>
2019-02-18 12:24:44 -0400

    Validacion de contrasenas nuevas que sean iguales, que no esten vacias, que sean mayor de 7 caracteres y menor de 15. Validacion del email de recuperacion, que sea un email valido, que este en nuestros registros y que no este vacio el campo. Mejora al modificar la contrasena consultar de nuevo el codigo de verificacion

--- VenusDoors/Scripts/js/PasswordRecovery.Logins.js
+++ VenusDoors/Scripts/js/PasswordRecovery.Logins.js
@@ -1,14 +1,72 @@
 ﻿$(document).ready(function () {
-    $("#btSend").on("click", function () {
-        EmailToRecuperation();
+    $("#btSend").on("click", function () {       
+        if (ValidarEmailVacio()) {
+            if(validarEmailvalido()){
+            $('#inptEmail').removeClass("is-invalid");
+            EmailToRecuperation();
+            }
+            else {
+                LlammarModal("Danger", "You must enter a valid email.");
+                $('#inptEmail').addClass("is-invalid");
+                $("#btSend").prop('disabled', false);
+            }
+        } else {
+            LlammarModal("Danger", "You must enter the Email.");
+            $('#inptEmail').addClass("is-invalid");
+            $("#btSend").prop('disabled', false);
+        }
     });
 
-    $("#verificationBTN").on("click", function () {
-        ValidateCodeRecovery();
+    $("#verificationBTN").on("click", function () {        
+        if (ValidarCodigoVacio()) {
+            $('#verificationCode').removeClass("is-invalid");
+            ValidateCodeRecovery();
+        } else {
+            LlammarModal("Danger", "You must enter the verification code.", " ");
+            $('#verificationCode').addClass("is-invalid");
+        }
     });
 
     $("#btNewppas").on("click", function () {
-        ModifyPassword();
+        if (ValidarCamposVacios()) {
+            if (ValidadCantidad()) {
+                if (ValContrasenas()) {
+                    ModifyPassword();
+                } else {
+                    LlammarModal("Danger", "Passwords do not match.", " ");
+                }
+            } else {
+                LlammarModal("Danger", "The password must have a minimum of 8 characters and a maximum of 15.");
+            }            
+        } else {
+            LlammarModal("Danger", "You must fill all the fields.", " ");
+        }            
+    });
+   
+    $(document).on('change', '#inptPassword1', function () {
+        if ($('#inptPassword1').val().length < 8) {
+            $('#inptPassword1').addClass("is-invalid"); 
+            $("#labelerror1").css('display', 'block');
+        } else if ($('#inptPassword1').val().length > 15) {
+            $('#inptPassword1').addClass("is-invalid");
+            $("#labelerror11").css('display', 'block');        
+        } else {
+            $('#inptPassword1').removeClass("is-invalid");
+            $("#labelerror1").css('display', 'none');
+        }        
+    });
+
+    $(document).on('change', '#inptPassword2', function () {
+        if ($('#inptPassword2').val().length < 8) {
+            $('#inptPassword2').addClass("is-invalid");
+            $("#labelerror2").css('display', 'block');
+        } else if ($('#inptPassword2').val().length > 15) {
+            $('#inptPassword2').addClass("is-invalid");
+            $("#labelerror22").css('display', 'block');
+        } else {
+            $('#inptPassword2').removeClass("is-invalid");
+            $("#labelerror2").css('display', 'none');
+        }
     });
 });
 
@@ -28,12 +86,14 @@ function EmailToRecuperation() {
 
             //Validar data para ver si mostrar error al guardar o exito al guardar
             if (result == 1) {
-                 LlammarModal("Danger", "Error! There is not a registered user with that email", "Check that it is well written and try again");
+                LlammarModal("Danger", "Error! There is not a registered user with that email", "Check that it is well written and try again");
+                $("#btSend").prop('disabled', false);
             } else if (result == 2) {
                 LlammarModal("Congratuletions", "We have sent an email with a verification code so you can modify your password.");                
                 setTimeout(function () { window.location.href = '/Logins/New_Password'; }, 3000);
             } else {
-                LlammarModal("Danger", "An error occurred during the process.", "Check your internet connection I tried again");
+                LlammarModal("Danger", "An error occurred during the process.");
+                $("#btSend").prop('disabled', false);
             }
         },
         error: function (err) {
@@ -62,8 +122,10 @@ function ValidateCodeRecovery() {
             //Validar data para ver si mostrar error al guardar o exito al guardar
             if (result == 1) {
                 $('#ModalVerificationCode').modal('hide');
+                $(".ddbbld").prop('disabled', false);
             } else if(result == 2){
                 LlammarModal("Danger", "The verification code is wrong");
+                $('#verificationCode').addClass("is-invalid");
             }else {
                 LlammarModal("Danger", "An error occurred during the process.");
             }
@@ -81,6 +143,8 @@ function ModifyPassword() {
             pusernew: {
                 Password: $('#inptPassword2').val(),
             },
+
+            verificationC: $('#verificationCode').val(),
         };
     $.ajax({
         type: 'POST',
@@ -91,9 +155,11 @@ function ModifyPassword() {
         success: function (result) {
 
             //Validar data para ver si mostrar error al guardar o exito al guardar
-            if (result == true) {
+            if (result == 1) {
                 LlammarModal("Congratuletions", "Your password has been modified successfully.");
                 setTimeout(function () { window.location.href = '/Logins/Index'; }, 3000);
+            } else if(result == 2){
+                LlammarModal("Danger", "Wait, something is not right?.", "We have detected a problem during the process. Have you skipped a step?");
             } else {
                 LlammarModal("Danger", "An error occurred during the process.");
             }
@@ -103,3 +169,86 @@ function ModifyPassword() {
         },
     });
 }
+
+function ValContrasenas() {
+    var aux = true;
+    if ($('#inptPassword1').val() != $('#inptPassword2').val()) {
+        $('#inptPassword1').addClass("is-invalid");
+        $('#inptPassword2').addClass("is-invalid");
+        $('#inptPassword1').val(""); $('#inptPassword2').val("");
+        aux = false;
+    } else {
+        $('#inptPassword1').removeClass("is-invalid");
+        $('#inptPassword2').removeClass("is-invalid");
+    }
+    return aux;
+}
+
+function ValidarCamposVacios() {
+    var aux = true; 
+    if ($('#inptPassword1').val() == "") {
+        $('#inptPassword1').addClass("is-invalid");
+        aux = false;
+    } else {
+        $('#inptPassword1').removeClass("is-invalid");
+    }
+    if ($('#inptPassword2').val() == "") {
+        $('#inptPassword2').addClass("is-invalid");
+        aux = false;
+    } else {
+        $('#inptPassword2').removeClass("is-invalid");
+    }
+    return aux;
+}
+
+function ValidarCodigoVacio() {
+    var aux = true;
+    if ($('#verificationCode').val() == "") {
+        $('#verificationCode').addClass("is-invalid");
+        aux = false;
+    } else {
+        $('#verificationCode').removeClass("is-invalid");
+    }    
+    return aux;
+}
+
+function ValidarEmailVacio() {
+    var aux = true;
+    if ($('#inptEmail').val() == "") {
+        $('#inptEmail').addClass("is-invalid");
+        aux = false;
+    } else {
+        $('#inptEmail').removeClass("is-invalid");
+    }
+    return aux;
+}
+
+function ValidadCantidad() {
+    var aux = true;
+    if ($('#inptPassword1').val().length < 8 || $('#inptPassword1').val().length > 15) {
+        $('#inptPassword1').addClass("is-invalid");
+        aux = false;
+    } else {
+        $('#inptPassword1').removeClass("is-invalid");
+    }
+    if ($('#inptPassword2').val().length < 8 || $('#inptPassword2').val().length > 15) {
+        $('#inptPassword2').addClass("is-invalid");
+        aux = false;
+    } else {
+        $('#inptPassword2').removeClass("is-invalid");
+    }
+    return aux;
+}
+
+function validarEmailvalido(){
+    var aux = true;
+    var regex = /^(?:[^<>()[\].,;:\s@"]+(\.[^<>()[\].,;:\s@"]+)*|"[^\n"]+")@(?:[^<>()[\].,;:\s@"]+\.)+[^<>()[\]\.,;:\s@"]{2,63}$/i;
+  
+    if (!regex.test($('#inptEmail').val())) {
+        $('#inptEmail').addClass("is-invalid");
+        aux = false;
+    } else {
+        $('#inptEmail').removeClass("is-invalid");
+    }
+    return aux;
+} 
