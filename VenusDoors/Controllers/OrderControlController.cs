81cdd10e4bf9c378e8cc0cfabab1678869894e67
gregorisalazar <gregorisalazar.a@gmail.com>
2019-02-19 11:55:31 -0400

    Master de company

---
+++ VenusDoors/Controllers/OrderControlController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace VenusDoors.Controllers
+{
+    public class OrderControlController : Controller
+    {
+        // GET: OrderControl
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
