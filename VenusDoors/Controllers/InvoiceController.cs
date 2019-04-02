13af285fe7ead1c1e0eb5d192e8aeb9f0ad6e1d2
xavierso <javier.sotillo13@gmail.com>
2019-04-02 15:30:04 -0400

    Diseño de la vista Invoice, Modificacion de la palabra Masters dependiendo del upline. Controlador de la vista Invoice, Estimate y Payments Received.

---
+++ VenusDoors/Controllers/InvoiceController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace VenusDoors.Controllers
+{
+    public class InvoiceController : Controller
+    {
+        // GET: Invoice
+        [Authorize(Roles = "1, 2")]
+        public ActionResult Index()
+        {
+            if (Session["UserID"] != null && (int)Session["UserType"] == 1)
+            {
+                ViewBag.Sales = "active show-sub";
+                ViewBag.Invoice = "active";
+                
+                return View();
+            }
+            else
+            {
+
+                return RedirectToAction("Index", "Home");
+            }
+        }
+    }
+}
