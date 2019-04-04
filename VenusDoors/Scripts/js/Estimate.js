25612f1fcb0145bc67e5a1871d74fb614f1f7320
xavierso <javier.sotillo13@gmail.com>
2019-04-04 13:24:51 -0400

    Estimate js

---
+++ VenusDoors/Scripts/js/Estimate.js
@@ -0,0 +1,39 @@
+﻿$(function () {
+    $("#add").click(function () {
+        var n = $('tr:last', $("#mitabla")).length;
+        var tds = '<tr>';
+        for (var i = 0; i < n; i++) {
+            tds += '<td> </td>';
+            tds += '<td><div contenteditable></div></td>';
+            tds += '<td><div contenteditable></div></td>';
+            tds += '<td><div contenteditable></div></td>';
+            tds += '<td><div contenteditable></div></td>';
+            tds += '<td><div contenteditable></div></td>';
+            tds += '<td></td>';
+        }
+        tds += '</tr>';
+        $("#mitabla").append(tds);
+    });
+
+    $('#modalToggle').click(function () {
+        $('#modal').modal({
+            backdrop: 'static'
+        });
+    });
+
+})
+$(function () {
+    'use strict';
+    $('.br-mailbox-list').perfectScrollbar();
+
+    $('#showMailBoxLeft').on('click', function (e) {
+        e.preventDefault();
+        if ($('body').hasClass('show-mb-left')) {
+            $('body').removeClass('show-mb-left');
+            $(this).find('.fa').removeClass('fa-arrow-left').addClass('fa-arrow-right');
+        } else {
+            $('body').addClass('show-mb-left');
+            $(this).find('.fa').removeClass('fa-arrow-right').addClass('fa-arrow-left');
+        }
+    });
+});
