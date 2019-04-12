b8821ae1ac3eacce1f28b20f06bf9382cb66a321
gregorisalazar <gregorisalazar.a@gmail.com>
2019-04-12 09:22:57 -0400

    invoices cambios

--- VenusDoors/Controllers/InvoiceController.cs
+++ VenusDoors/Controllers/InvoiceController.cs
@@ -3,26 +3,29 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Model;
 
 namespace VenusDoors.Controllers
 {
     public class InvoiceController : Controller
     {
         // GET: Invoice
-        [Authorize(Roles = "1, 2")]
+        [Authorize(Roles = "1")]
         public ActionResult Index()
-        {
-            if (Session["UserID"] != null && (int)Session["UserType"] == 1)
+        {          
+            try
             {
                 ViewBag.Sales = "active show-sub";
                 ViewBag.Invoice = "active";
-                
+                //BusinessLogic.lnInvoice _LNINV = new BusinessLogic.lnInvoice();
+                //List<Invoice> est = _LNINV.GetInvoice(0,0, (int)Session["IdCompany"], 0, 0, 0, DateTime.Parse("1999/01/01"), DateTime.Now.AddDays(1)).OrderByDescending(x => x.IdFolio).ToList();
+                //var serializar1 = new System.Web.Script.Serialization.JavaScriptSerializer();
+                //ViewBag.Invoice = serializar1.Serialize(est);
                 return View();
             }
-            else
+            catch (Exception)
             {
-
-                return RedirectToAction("Index", "Home");
+                return View("Error");
             }
         }
     }
