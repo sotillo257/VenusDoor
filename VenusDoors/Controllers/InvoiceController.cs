716163c4a77ff3a15a1d6c5b74b84c2de856e5af
gregorisalazar <gregorisalazar.a@gmail.com>
2019-04-15 09:02:19 -0400

    cambios en invoice

--- VenusDoors/Controllers/InvoiceController.cs
+++ VenusDoors/Controllers/InvoiceController.cs
@@ -17,15 +17,32 @@ namespace VenusDoors.Controllers
             {
                 ViewBag.Sales = "active show-sub";
                 ViewBag.Invoice = "active";
-                //BusinessLogic.lnInvoice _LNINV = new BusinessLogic.lnInvoice();
-                //List<Invoice> est = _LNINV.GetInvoice(0, 0, (int)Session["IdCompany"], 0, 0, 0, DateTime.Parse("1999/01/01"), DateTime.Now.AddDays(1)).OrderByDescending(x => x.IdFolio).ToList();
-                //var serializar1 = new System.Web.Script.Serialization.JavaScriptSerializer();
-                //ViewBag.Invoice = serializar1.Serialize(est);
+                BusinessLogic.lnInvoice _LNINV = new BusinessLogic.lnInvoice();
+                List<Invoice> est = _LNINV.GetInvoice(0, 0, (int)Session["IdCompany"], 0, 0, 0, DateTime.Parse("1999/01/01"), DateTime.Now.AddDays(1)).OrderByDescending(x => x.IdFolio).ToList();
+                var serializar1 = new System.Web.Script.Serialization.JavaScriptSerializer();
+                ViewBag.Invoice = serializar1.Serialize(est);
                 return View();
             }
             catch (Exception)
             {
                 return View("Error");
+            }
+        }
+
+        [Authorize]
+        [HttpPost]
+        public ActionResult GetHistoryInvoice(int idInvoice)
+        {
+            try
+            {
+                BusinessLogic.lnHistoryInvoice _LNHIST = new BusinessLogic.lnHistoryInvoice();
+                List<HistoryInvoice> list = _LNHIST.GetHistoryInvoiceByIdInvoice(idInvoice);
+                return Json(new { listHistory = list, Success = true, Mensaje = "" }, JsonRequestBehavior.AllowGet);
+
+            }
+            catch (Exception ex)
+            {
+                return Json(new { Success = false, Mensaje = ex.Message }, JsonRequestBehavior.AllowGet);
             }
         }
     }
